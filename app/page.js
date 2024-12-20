e6d64f28bd6a71f682019651db006bbcf233351d
ShaYri12 <sharjeelb60@gmail.com>
2024-12-21 00:26:02 +0500

    code optimized

--- app/page.js
+++ app/page.js
@@ -1,11 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Home/Hero";
-import CreditProducts from "./components/Home/CreditProducts";
-import FinancialSlider from "./components/Home/FinancialSlider";
 import Footer from "./components/Footer";
 import Partners from "./components/Home/Partners";
-import TailorMadeProducts from "./components/Home/TailorMadeProducts";
-import InsuranceInvestment from "./components/Home/InsuranceInvestment";
 import MeriMudraWorks from "./components/Home/MeriMudraWorks";
 import MyProduct from "./components/Home/MyProduct";
 import LoanCategories from "./components/Home/LoanCategories";
@@ -20,10 +16,6 @@ export default function Home() {
     <main className="min-h-screen bg-white overflow-x-hidden">
       <Navbar />
       <Hero />
-      {/* <CreditP/>roducts /> */}
-      {/* <InsuranceInvestment /> */}
-      {/* <FinancialSlider /> */}
-      {/* <TailorMadeProducts /> */}
       <MyProduct />
       <LoanCategories />
       <CarLoans />
