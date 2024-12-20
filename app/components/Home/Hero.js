c32ddaabd18f3eecc422b0d5e53ee90a26ddb46f
ShaYri12 <sharjeelb60@gmail.com>
2024-12-21 00:48:45 +0500

    hero responsive done

--- app/components/Home/Hero.js
+++ app/components/Home/Hero.js
@@ -9,14 +9,14 @@ import { IoIosArrowForward } from "react-icons/io";
 const sliderData = [
   {
     title: (
-      <h1 className="text-[28px] font-[700] text-[#60bb51] leading-[44px] mb-[15px]">
-        Your Credit Score & Report <br />
+      <h1 className="text-[13px] lg:text-[28px] font-[700] text-[#60bb51] lg:leading-[44px] lg:mb-[15px]">
+        Your Credit Score & Report <br className="sm:flex hidden" />{" "}
         <span className="line-through">worth ₹1,200</span>{" "}
         <span className="text-[#60bb51]">Absolutely FREE</span>
       </h1>
     ),
     description: (
-      <p className="mb-[25px] text-base text-[#052f5] opacity-[0.6]">
+      <p className="mb-[10px] lg:mb-[25px] text-[10px] lg:text-base text-[#052f5] opacity-[0.6]">
         Your credit score is more than just a number. It's the key to help you
         unlock the doors to the best loans & credit card offers available.
       </p>
@@ -26,19 +26,19 @@ const sliderData = [
   },
   {
     title: (
-      <h1 className="text-[28px] font-[700] text-[#60bb51] leading-[44px] mb-[15px]">
-        Proccessing fee @0 <br />
+      <h1 className="text-[13px] lg:text-[28px] font-[700] text-[#60bb51] lg:leading-[44px] lg:mb-[15px]">
+        Proccessing fee @0 <br className="sm:flex hidden" />{" "}
         <span className="line-through"> processing fee</span>
         <span className="text-[#60bb51]"> Absolutely FREE</span>
       </h1>
     ),
     description: (
-      <p className="mb-[25px] text-base text-[#052f5] opacity-[0.6]">
+      <p className="mb-[10px] lg:mb-[25px] text-[10px] lg:text-base text-[#052f5] opacity-[0.6]">
         Your credit score is more than just a number. It's the key to help you
         unlock the doors to the best loans & credit card offers available.
       </p>
     ),
-    cta: "Get Free Credit Report",
+    cta: "Apply Now",
     image: "/assets/slider1.png",
   },
 ];
@@ -66,8 +66,8 @@ export default function Hero() {
 
   return (
     <div
-      className="relative h-[460px] pt-[100px] mb-[20px] border-b-[3px] border-[#e8eef8] flex items-center justify-center"
-      style={{ background: "linear-gradient(180deg, #fff 0, #f9fbff 100%)" }}
+      className="relative lg:h-[460px] pt-[80px] lg:pt-[100px] mb-[20px] border-b-[3px] border-[#e8eef8] flex items-center justify-center"
+      style={{ background: "linear-gradient(180deg, #fff 0, #f3f7ff 100%)" }}
     >
       <Slider
         {...settings}
@@ -76,28 +76,26 @@ export default function Hero() {
         {sliderData.map((slide, index) => (
           <div
             key={index}
-            className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8 xl:px-0 h-full w-full flex items-center justify-center"
+            className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8 xl:px-0 lg:py-0 py-4 h-full w-full flex items-center justify-center"
           >
-            <div className="lg:grid lg:grid-cols-12 lg:gap-[50px] h-full w-full">
-              <div className="lg:col-span-5 flex items-center justify-center">
-                <div className="relative h-[320px] w-full">
+            <div className="grid grid-cols-12 lg:gap-[50px] gap-4 h-full w-full">
+              <div className="col-span-5 flex items-center justify-center">
+                <div className="relative lg:h-[320px] w-full">
                   <img
                     src={slide.image}
                     alt="Credit Score Illustration"
-                    // layout="fill"
-                    // objectFit="contain"
                     priority
                   />
                 </div>
               </div>
-              <div className="mt-12 lg:mt-0 lg:col-span-7 flex items-center justify-center lg:justify-start">
-                <div className="text-center lg:text-left">
+              <div className="col-span-7 flex items-center justify-center lg:justify-start">
+                <div className="text-left">
                   {slide.title}
                   {slide.description}
                   <div className="">
                     <a
                       href="#"
-                      className="inline-flex items-center justify-center gap-2 ps-[32px] pe-[28px] py-[18px] text-base font-[600] rounded-[8px] text-white bg-[#60bb51] "
+                      className="inline-flex items-center justify-center gap-1 lg:gap-2 ps-[10px] lg:ps-[32px] lg:pe-[28px] pe-[10px] py-[4px] lg:py-[18px] text-[11px] sm:text-[12px] lg:text-base font-[600] rounded-[8px] text-white bg-[#60bb51] "
                     >
                       {slide.cta}
                       <IoIosArrowForward className="text-white" />
